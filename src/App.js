824ffa550eb5d30101dafeac6841b5657fc85d60
rmoham <riyaz.ge@gmail.com>
2023-11-05 12:59:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import About from "./componenets/About";
+import Footer from "./componenets/Footer";
+import Hero from "./componenets/Hero";
+import Navbar from "./componenets/Navbar";
+import Services from "./componenets/Services";
+import Tours from "./componenets/Tours";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+export default App;
